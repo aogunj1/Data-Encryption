369a57711f08bf04910c648a127c669cc526f69f
aogunj1 <86618768+aogunj1@users.noreply.github.com>
2023-03-28 15:11:17 -0400

    switch to typescript
    it was built for javascript now its typescript

--- encryption/src/App.js
+++ encryption/src/App.js
@@ -1,22 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import crypto from 'crypto';
+import * as CryptoJS from "crypto-js";
 function App() {
+  const handleClick = () => {
+    console.log('Button clicked');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
+        <button onClick={handleClick}>Click here to generate Encyption Key</button>
       </header>
     </div>
   );
